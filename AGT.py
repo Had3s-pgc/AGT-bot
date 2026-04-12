ab935bad4ab9cc4040d93fde76ec5fe2ff0fca59
Had3s-pgc <hady2011111@gmail.com>
2026-04-12 17:43:23 -0500

    Update AGT.py

--- AGT.py
+++ AGT.py
@@ -238,8 +238,8 @@ async def info(interaction: discord.Interaction):
             "* Lists the current teams playing within the league.<<<\n"
             "\n"
             "-# PGC Season Management System - Created by Had3s"), color=0x15D466)
-    await interaction.response.send_message(embed=embed, ephemeral=True) # When sending the information of the slash commands in a server change the "✅ Done" to embed=embed and remove the await interaction.channel.send(embed=embed) AFTER you send the info
-
+    await interaction.response.send_message("done", ephemeral=True) # When sending the information of the slash commands in a server change the "✅ Done" to embed=embed and remove the await interaction.channel.send(embed=embed) AFTER you send the info
+    interaction.channel.send(embed=embed)
 # /print
 @client.tree.command(name="print", description="Re-say what you say", guild=GUILD_ID)
 async def printing(interaction: discord.Interaction, printing: str):
